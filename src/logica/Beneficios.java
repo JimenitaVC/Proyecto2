ffc85420ef2c447c3a7aebacb55af3d3e0b6cfe9
Admin <Admin@JIMENITAGUAPA>
2025-08-18 19:10:26 -0600

    se agrega la información de las clase Beneficios y BeneficiosEstudiantes

--- src/logica/Beneficios.java
+++ src/logica/Beneficios.java
@@ -4,10 +4,55 @@
  */
 package logica;
 
-/**
- *
- * @author Admin
- */
+import java.math.BigDecimal;
 public class Beneficios {
-    
+     private int idBeneficio;
+    private String nomBeneficio;
+    private String descripcion;
+    private BigDecimal montoBeneficio;
+
+    public Beneficios(int idBeneficio, String nomBeneficio, String descripcion, BigDecimal montoBeneficio) {
+        this.idBeneficio = idBeneficio;
+        this.nomBeneficio = nomBeneficio;
+        this.descripcion = descripcion;
+        this.montoBeneficio = montoBeneficio;
+    }
+        public Beneficios() {
+        this.idBeneficio = 0;
+        this.nomBeneficio = "";
+        this.descripcion = "";
+        this.montoBeneficio = null;
+    }
+
+    public int getIdBeneficio() {
+        return idBeneficio;
+    }
+
+    public void setIdBeneficio(int idBeneficio) {
+        this.idBeneficio = idBeneficio;
+    }
+
+    public String getNomBeneficio() {
+        return nomBeneficio;
+    }
+
+    public void setNomBeneficio(String nomBeneficio) {
+        this.nomBeneficio = nomBeneficio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getMontoBeneficio() {
+        return montoBeneficio;
+    }
+
+    public void setMontoBeneficio(BigDecimal montoBeneficio) {
+        this.montoBeneficio = montoBeneficio;
+    }
 }
--- src/logica/BeneficiosEstudiantes.java
+++ src/logica/BeneficiosEstudiantes.java
@@ -4,10 +4,33 @@
  */
 package logica;
 
-/**
- *
- * @author Admin
- */
+
 public class BeneficiosEstudiantes {
-    
+  private int cedula;
+    private int idBeneficio;
+
+    public BeneficiosEstudiantes(int cedula, int idBeneficio) {
+        this.cedula = cedula;
+        this.idBeneficio = idBeneficio;
+    }
+        public BeneficiosEstudiantes() {
+        this.cedula = 0;
+        this.idBeneficio = 0;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public int getIdBeneficio() {
+        return idBeneficio;
+    }
+
+    public void setIdBeneficio(int idBeneficio) {
+        this.idBeneficio = idBeneficio;
+    }  
 }
